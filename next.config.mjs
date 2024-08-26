feb38b11a95d71100ee49ac21f217ada75e6e3b8
pyc <pyc@mykitties.life>
2024-08-26 13:23:38 +0800

    chore: format config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.svg$/i,
-            issuer: /\.[jt]sx?$/,
-            use: ['@svgr/webpack'],
-          })
+output: 'export',
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
 
-          return config
-    }
+    return config
+  }
 };
 
 export default nextConfig;
