81882e1840533f9e78bc301fb64262b24727f510
pyc <pyc@mykitties.life>
2024-08-26 16:10:46 +0800

    fix: fix mobile layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ProductsSlider from './_components/ProductsSlider';
 
 export default function Home() {
   return (
-    <main className="p-4">
+    <main className="p-4 overflow-hidden">
       {/* 如果有要用 streaming 的方式的話，這邊應該用 skeleton 之類的東西 */}
       <div className="hidden md:block">
         <h1 className="font-bold mb-2 text-center text-5xl">Best Seller Gaming PC</h1>
